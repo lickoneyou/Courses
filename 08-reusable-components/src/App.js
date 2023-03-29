95a18fbc03f0895cd8f9d3ed152c45eb9df1d5ee
Kiryll Khomchanka <dickdurex265@gmail.com>
2023-03-29 15:22:03 +0300

    completed task 8

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+import MyComponent from './components/Mycomponent'
+import OtherComponent from './components/OtherComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent></MyComponent>
+      <OtherComponent></OtherComponent>
+      <MyComponent></MyComponent>
+      <OtherComponent></OtherComponent>
+      <MyComponent></MyComponent>
+    </div>
+  )
+}
+
+export default App
---
+++ 08-reusable-components/src/components/Mycomponent.js
@@ -0,0 +1,10 @@
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello from reusable component</h1>
+      <button>Like!</button>
+    </div>
+  )
+}
+
+export default MyComponent
