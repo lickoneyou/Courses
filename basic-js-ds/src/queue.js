11c95af6f7dd18669e9b2e0f78e0aad1f7529877
Кирилл Хомченко <dickdurex265@gmail.com>
2023-04-05 15:59:17 +0300

    added tasks

---
+++ basic-js-ds/src/queue.js
@@ -0,0 +1,36 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+// const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+class Queue {
+
+  getUnderlyingList() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  enqueue(/* value */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  dequeue() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+}
+
+module.exports = {
+  Queue
+};
