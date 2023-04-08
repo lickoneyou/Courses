73d04bcd5f652349b4b4c03a7006791c06c67881
Kiryll Khomchanka <dickdurex265@gmail.com>
2023-04-08 20:02:33 +0300

    completed Guessing game task

--- guessing-game/src/guessing-game.js
+++ guessing-game/src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0
+    this.max = 0
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min
+    this.max = max
+  }
 
-    }
+  guess() {
+    return Math.ceil((this.max + this.min) / 2)
+  }
 
-    guess() {
+  lower() {
+    this.max = Math.ceil((this.max + this.min) / 2)
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = Math.ceil((this.max + this.min) / 2)
+  }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
