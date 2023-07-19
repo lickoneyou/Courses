7c528334d67fb956858a4a73a5b07606384b5fb7
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-07-19 16:13:54 +0300

    completed calc

---
+++ calc/script.js
@@ -0,0 +1,17 @@
+const calc = {
+  sum(a, b) {
+    return a + b
+  },
+  sub(a, b) {
+    return a - b
+  },
+  div(a, b) {
+    return a / b
+  },
+  mult(a, b) {
+    return a * b
+  },
+  exp(a, b) {
+    return a ** b
+  },
+}
