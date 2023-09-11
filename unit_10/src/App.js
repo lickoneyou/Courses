e6226cf361d7d098a83687fea3cbfe2ac2ee49f2
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-09-11 21:28:21 +0300

    completed task 10

---
+++ unit_10/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import In from './In'
+import Inp from './Inp'
+import Out from './Out'
+
+function App() {
+  return (
+    <div className="App">
+      <In />
+      <Out />
+      <Inp />
+    </div>
+  )
+}
+
+export default App
