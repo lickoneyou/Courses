fe5991a85cc8bd0ff4480fb112e1a67292d6cc84
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-02-16 16:05:35 +0300

    completed AverageHumanAge task

---
+++ modernJS/AverageHumanAge/script.js
@@ -0,0 +1,13 @@
+const getAverageHumanAge = (catAges) => {
+  const filterArr = catAges
+    .map((el) => (el <= 2 ? el * 10 : el * 7))
+    .filter((el) => el > 18);
+  const filterArrLength = filterArr.length;
+  return filterArr.reduce((acc, el) => acc + el, 0) / filterArrLength;
+};
+
+const arr1 = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
+const arr2 = [1, 16, 12, 4, 5, 1, 3, 11, 7, 2];
+
+console.log(getAverageHumanAge(arr1));
+console.log(getAverageHumanAge(arr2));
